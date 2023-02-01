8ad2895ef0e330b3616c80c832951798d4173c55
Arthur Bazaev <azun7210@gmail.com>
2023-02-01 14:53:23 +0300

    поиск корней

--- testJsFile2.js
+++ testJsFile2.js
@@ -1,20 +1,41 @@
-function task1(){
-    let num=prompt('Введите число:');
-    alert(`Периметр:${num*4}
-        \n Площадь: ${num*num}`);
+function Task1(){
+    let input1=document.createElement('input');
+    let input2=document.createElement('input');
+    let input3=document.createElement('input');
+
+    input1.id='id1';
+    input2.id='id2';
+    input3.id='id3';
+
+    let button1 =document.createElement('button');
+
+    button1.style.width='100px';
+    button1.style.height='20px';
+
+    document.body.append(input1);
+    document.body.append(input2);
+    document.body.append(input3);
+    document.body.append(button1);
+    
+    button1.onclick=Event_;
+        
+    
 }
-function task2(){
-    let num1=prompt('Введите длинну:');
-    let num2=prompt('Введите высоту:');
-    alert(`Периметр:${num1*2+num2*2}
-        \n Площадь: ${num1*num2}`);
-}
-function task3(){
-    let num=prompt('Введите радиус:');
-    alert(`Периметр:${2*Math.PI*num}
-    \n Площадь: ${Math.PI*num*num}`);
-}
-function Task4(){
-    let a=prompt('Введите a:'),b=prompt('Введите b:'),c=prompt('Введите c:');
-    let p=Ma
+function Event_()
+{
+    let a= parseInt(document.getElementById('id1').value);
+    let b= parseInt(document.getElementById('id2').value);
+    let c= parseInt(document.getElementById('id3').value);
+    let d=b*b-4*a*c;
+    if(d<0){
+        alert('нет корней');
+    }
+    else if(d==0)
+    {
+        alert(`уравнение имеет оди корень: ${-b/(2*a)}`);
+    }
+    else if(d>0)
+    {
+        alert(`x1: ${(-b-Math.sqrt(d))/(2*a)} x2:${(-b+Math.sqrt(d))/(2*a)}`);
+    }
 }
--- testJsFile5.js
+++ testJsFile5.js
@@ -1,5 +1,15 @@
 function task1() {
-    // заменим цвет фона на красный,
-    alert(document.body.children[0]);
+    let table = document.body.firstElementChild;
+
+    for (let i = 0; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        row.cells[i].style.backgroundColor = 'red';
+    }
+
+
+}
+function task2() {
+    let elemnt = search.querySelectorAll('input');
+    console.log(elemnt[elemnt.length-1]);
 }
 
